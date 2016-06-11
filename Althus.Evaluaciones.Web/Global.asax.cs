146d39bf94e8cc9d77e053fc04a06af1ad1c4be4
ngiannetti@metricarts.com <ngiannetti@metricarts.com>
2016-06-11 19:03:05 -0400

    ADDED datacontext ADDED simplemembership

--- Althus.Evaluaciones.Web/Global.asax.cs
+++ Althus.Evaluaciones.Web/Global.asax.cs
@@ -25,6 +25,7 @@ namespace Althus.Evaluaciones.Web
 
             IList<IConfigurable> configurations = new List<IConfigurable>();
             configurations.Add(new MapperConfiguration());
+            configurations.Add(new InitSimpleMembership());
             foreach (var configuration in configurations)
             {
                 configuration.Configure();
--- Althus.Evaluaciones.Web/Models/IdentityModels.cs
+++ Althus.Evaluaciones.Web/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Althus.Evaluaciones.Web.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("AppDb", throwIfV1Schema: false)
         {
         }
 
