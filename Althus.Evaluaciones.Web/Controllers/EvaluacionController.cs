0b1661e16cd19fc21714ee1eab5745e7442ffbb3
xruizv <xruizv@gmail.com>
2016-06-12 19:02:19 -0400

    listar con bug

--- Althus.Evaluaciones.Web/Controllers/EvaluacionController.cs
+++ Althus.Evaluaciones.Web/Controllers/EvaluacionController.cs
@@ -5,15 +5,20 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using PagedList;
+using Althus.Evaluaciones.Core;
 
 namespace Althus.Evaluaciones.Web.Controllers
 {
     public class EvaluacionController : BaseController
     {
-        public ActionResult ListadoEvaluaciones(int? pagina)
+        public ActionResult ListadoEvaluaciones(int? pagina, ListadoEvaluacionesFormModel FORM)
         {
-            ListadoEvaluacionesViewModel model = new ListadoEvaluacionesViewModel();
-            model.Evaluaciones = db.Evaluacions.ToPagedList(pagina ?? 1, 10);
+            ListadoEvaluacionesViewModel model = new ListadoEvaluacionesViewModel(FORM,db);
+            IQueryable<Evaluacion> items = db.Evaluacions;
+            if(ModelState.IsValid){
+            }
+            model.Evaluaciones = items.OrderByDescending(x=> x.FechaEvaluacion)
+                .ToPagedList(pagina ?? 1, 10);
             return View(model);
         }
 
---
+++ Althus.Evaluaciones.Web/Models/EvaluacionModels/ListadoEvaluacionesFormModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Althus.Evaluaciones.Web.Models.EvaluacionModels
+{
+    public class ListadoEvaluacionesFormModel
+    {
+        [DisplayName("Empresa")]
+        public int IdEmpresa { get; set; }
+
+        [DisplayName("Cargo")]
+        public int IdCargo { get; set; }
+    }
+}
