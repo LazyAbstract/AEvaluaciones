c6039242391906ccc12d812f710af19522dd2211
xruizv <xruizv@gmail.com>
2016-06-12 16:48:11 -0400

    first table document evvaluacion

--- Althus.Evaluaciones.Web/Models/EvaluacionModels/GeneraEvaluacionPDF.cs
+++ Althus.Evaluaciones.Web/Models/EvaluacionModels/GeneraEvaluacionPDF.cs
@@ -11,12 +11,12 @@ namespace Althus.Evaluaciones.Web.Models.EvaluacionModels
 {
     public class GeneraEvaluacionPDF
     {
-        //private Evaluacion _Evaluacion { get; set; }
+        private Evaluacion _Evaluacion { get; set; }
         private byte[] _File { get; set; }
 
-        public GeneraEvaluacionPDF()//(Evaluacion evaluacion)
+        public GeneraEvaluacionPDF(Evaluacion evaluacion)//(Evaluacion evaluacion)
         {
-            //_Evaluacion = evaluacion;
+            _Evaluacion = evaluacion;
             GenerateFile();
         }
 
@@ -36,7 +36,30 @@ namespace Althus.Evaluaciones.Web.Models.EvaluacionModels
 
                 //Generación del contenido
                 document.Open();
+                PdfPTable firstTable = GetPdfTable(2);
+                firstTable.AddCell(GetHeaderCell("Nombre"));
 
+                firstTable.AddCell(GetHeaderCell("Cargo al cual postula"));
+                firstTable.AddCell(GetNormalCell(_Evaluacion.Evaluado.Nombre));                
+                firstTable.AddCell(GetHeaderCell("Empresa"));
+                firstTable.AddCell(GetNormalCell(""));  
+                firstTable.AddCell(GetHeaderCell("Profesión"));
+                firstTable.AddCell(GetNormalCell(_Evaluacion.Evaluado.Profesion)); 
+                DateTime today = DateTime.Today;
+                int age = today.Year - _Evaluacion.Evaluado.FechaNacimiento.Year;
+                if (_Evaluacion.Evaluado.FechaNacimiento > today.AddYears(-age))
+                    age--;
+                firstTable.AddCell(GetHeaderCell("Edad"));
+                firstTable.AddCell(GetNormalCell(age.ToString())); 
+                firstTable.AddCell(GetHeaderCell("Rut"));
+                firstTable.AddCell(GetNormalCell(new Rut(_Evaluacion.Evaluado.Rut).ToStringConGuion())); 
+                firstTable.AddCell(GetHeaderCell("Teléfonos"));
+                firstTable.AddCell(GetNormalCell(_Evaluacion.Evaluado.Celular.ToString()));
+                firstTable.AddCell(GetHeaderCell("E-mail"));
+                firstTable.AddCell(GetNormalCell(_Evaluacion.Evaluado.Correo));
+                firstTable.AddCell(GetHeaderCell("Fecha de Evaluación"));
+                firstTable.AddCell(GetNormalCell(_Evaluacion.FechaEvaluacion.ToShortDateString()));
+                document.Add(firstTable);
 
                 //Exportar archivo
                 document.Close();
@@ -46,5 +69,27 @@ namespace Althus.Evaluaciones.Web.Models.EvaluacionModels
             }
         }
 
+        private PdfPTable GetPdfTable(int numColumns) {
+            PdfPTable table = new PdfPTable(numColumns);
+            table.HorizontalAlignment = Rectangle.ALIGN_LEFT;
+            return table;
+        }
+
+        private PdfPCell GetHeaderCell(string texto){
+            Phrase frase = new Phrase(texto ?? String.Empty, headerFont);
+            PdfPCell cel = new PdfPCell(frase);
+            cel.BackgroundColor = BaseColor.GREEN;
+            return cel;
+        }
+
+        private PdfPCell GetNormalCell(string texto)
+        {
+            Phrase frase = new Phrase(texto ?? String.Empty, normalFont);
+            PdfPCell cel = new PdfPCell(frase);
+            return cel;
+        }
+
+        private const Font headerFont = new Font(Font.FontFamily.TIMES_ROMAN, 14, Font.BOLD, BaseColor.WHITE);
+        private const Font normalFont = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.NORMAL, BaseColor.BLACK);
     }
 }
