2ebd08f5928d3b0c7c0bce0f84e7643837fe9edc
xruizv <xruizv@gmail.com>
2016-06-12 22:13:40 -0400

    helpers

---
+++ Althus.Evaluaciones.Web/Helpers/ImageHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Althus.Evaluaciones.Web.Helpers
+{
+    public static class ImageHelpers
+    {
+        public static Image ScaleImage(Image image, int maxWidth, int maxHeight)
+        {
+            var ratioX = (double)maxWidth / image.Width;
+            var ratioY = (double)maxHeight / image.Height;
+            var ratio = Math.Min(ratioX, ratioY);
+
+            var newWidth = (int)(image.Width * ratio);
+            var newHeight = (int)(image.Height * ratio);
+
+            var newImage = new Bitmap(newWidth, newHeight);
+
+            using (var graphics = Graphics.FromImage(newImage))
+                graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
+            return newImage;
+        }
+    }
+}
